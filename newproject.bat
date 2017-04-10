f642b4cab75b8dcc365ce300b215891e933472fb
nicksoff <rusboy07@gmail.com>
2017-04-10 17:11:51 +0300

    Add files via upload

---
+++ newproject.bat
@@ -0,0 +1,23 @@
+@echo New Gulp Project
+@echo ________________
+@echo ________________
+@echo now will install gulp's libraries and making files/dir
+mkdir app 
+mkdir dist
+cd.>gulpfile.js
+cd app 
+cd.> index.html
+mkdir css
+mkdir js
+mkdir img
+cd css
+cd.>stylesheet.css
+cd..
+cd js
+cd.>script.js
+cd..
+cd..
+@echo Installing Gulp && npm install -g gulp && @echo INIT Gulp & npm init && @echo Install Gulp to current project & npm i gulp --save-dev && @echo Install SASS preprocessor & npm i gulp-sass --save-dev && @echo Install Browser-sync & npm i browser-sync --save-dev && @echo Install Bower & npm i -g bower && @echo Install jQuery and Magnific Popup& bower i jquery magnific-popup && @echo Install compressor of JS & npm i --save-dev gulp-concat gulp-uglifyjs && @echo Install compressor of CSS & npm i gulp-cssnano gulp-rename --save-dev && @echo Install package of FBuild & npm i del --save-dev && @echo Install package of IMG ZIP & npm i gulp-imagemin imagemin-pngquant --save-dev && @echo Install Img cache & npm i gulp-cache --save-dev  && @echo Install Autoprefix of CSS & npm i --save-dev gulp-autoprefixer && gulp
+@echo ________________
+@echo ________________
+pause
